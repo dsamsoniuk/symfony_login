7d5a87bb42614029a6303be75c711fa1b9a778b4
damian12345 <d.samsoniuk@protonmail.com>
2018-07-05 14:45:03 +0200

    szablon logowania

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+// src/Controller/DefaultController.php
+// ...
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function admin()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response('<html><body>gloan page!</body></html>');
+    }
+
+}
